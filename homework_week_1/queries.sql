cf76a9c60acdf98ddae65be269834eee922b9e8b
Orest Tokovenko <82840296+orestexemptedge@users.noreply.github.com>
2022-02-07 12:37:12 -0700

    sql file update

--- homework_week_1/queries.sql
+++ homework_week_1/queries.sql
@@ -1,21 +1,32 @@
-SELECT COUNT(*)
-FROM b_cust_table
-WHERE ACTIVE = 'N';
-# 574
+# Question 3
 
-SELECT COUNT(*)
-FROM b_cust_table
-WHERE ACTIVE = 'Y';
-# 10691
+SELECT date_trunc('day', tpep_pickup_datetime) AS day, COUNT(index)
+FROM yellow_taxi_data
+GROUP BY day
+ORDER BY day desc;
 
-SELECT FIRST_NAME, LAST_NAME, b_cust_sharing.ACTIVE
-FROM b_cust_sharing
-JOIN b_cust_table bct on b_cust_sharing.C_ID = bct.CUST_ID
-WHERE b_cust_sharing.ACTIVE = 'N';
+# Question 4
 
-SELECT DISTINCT M_STATUS
-FROM b_cust_table;
+SELECT date_trunc('day', tpep_pickup_datetime) AS day, MAX(tip_amount) AS largest_tip
+FROM yellow_taxi_data
+GROUP BY day
+ORDER BY largest_tip desc;
 
-SELECT *
-FROM b_cust_table
-WHERE EMPLOYER IS NULL;
+# Question 5
+
+SELECT COUNT(ytd.index) AS count, dropoff."Zone" AS dropoff_zone
+FROM yellow_taxi_data ytd
+    JOIN taxi_zones pickup on ytd."PULocationID" = pickup."LocationID"
+    JOIN taxi_zones dropoff on ytd."DOLocationID" = dropoff."LocationID"
+WHERE pickup."Zone" = 'Central Park' AND date_trunc('day', tpep_pickup_datetime) = '2021-01-14 00:00:00.000000'
+GROUP BY dropoff."Zone"
+ORDER BY count DESC;
+
+# Question 6
+
+SELECT pickup."Zone" AS pickup_zone, dropoff."Zone" AS dropoff_zone, AVG(total_amount) as avg_total_amount
+FROM yellow_taxi_data ytd
+    JOIN taxi_zones pickup on ytd."PULocationID" = pickup."LocationID"
+    JOIN taxi_zones dropoff on ytd."DOLocationID" = dropoff."LocationID"
+GROUP BY pickup."Zone", dropoff."Zone"
+ORDER BY AVG(total_amount) DESC;
